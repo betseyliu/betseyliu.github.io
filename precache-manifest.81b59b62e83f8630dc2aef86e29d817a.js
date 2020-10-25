62bb7b5e76ede74107c05ce892a4c5781caefa00
liubingjing.betsey <liubingjing.betsey@bytedance.com>
2020-10-25 14:26:15 +0800

    Updates

---
+++ precache-manifest.81b59b62e83f8630dc2aef86e29d817a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719edf382dded83198f53684f9a87a14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5b47e68375a96bba171",
+    "url": "/static/css/main.0591d36e.chunk.css"
+  },
+  {
+    "revision": "2143dc93519f29e9a95f",
+    "url": "/static/js/2.98c60e0a.chunk.js"
+  },
+  {
+    "revision": "0f9e60998cf325a3397879dbf7c5af2a",
+    "url": "/static/js/2.98c60e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b47e68375a96bba171",
+    "url": "/static/js/main.b4e6590d.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  }
+]);
